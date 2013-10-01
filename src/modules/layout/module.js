012b65a6b5f9de782e4a395fd800d1bc91e49aa3
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-01 23:05:52 +0200

    cleanup layout module and use menu
    This adds all the bits and pieces needed in the layout module to use
    the new menu infrastructure. The core changes where only the new
    modules. As with the menu module before, there are some kinks that
    still need working out and the code is already documented a bit.

--- src/modules/layout/module.js
+++ src/modules/layout/module.js
@@ -1,14 +1,22 @@
-/**
- * Layout Module handles header and footer contents
+/*
+ * # Layout
  *
- * @todo integrate AppModule properly so we can display a nice navi in the header.
+ * handles header and footer contents.
+ *
+ * The layout module takes care of adding some contents to the header and footer
+ * of the page.
  */
-Graviphoton.module('Layout', function(Layout, App, Backbone, Marionette, $, _, JST) {
+Graviphoton.module('Layout', function(Layout, App, Backbone, Marionette, $, _, Graviphoton, JST) {
 
-  Layout.Header = Backbone.Marionette.ItemView.extend({
+  /*
+   * ## Header
+   *
+   * The layout header has a region for the menu.
+   */
+  Layout.Header = Backbone.Marionette.Layout.extend({
     template: JST['layout/header'],
-    initialize: function() {
-      //this.listenTo(AppModuleCollection, "change", this.reset);
+    regions: {
+      menu: "#menu"
     }
   });
 
@@ -16,18 +24,37 @@ Graviphoton.module('Layout', function(Layout, App, Backbone, Marionette, $, _, J
     template: JST['layout/main'],
   });
 
+  /*
+   * ## Footer
+   *
+   * The footer is very simpl and only consists of a template.
+   */
   Layout.Footer = Backbone.Marionette.ItemView.extend({
     template: JST['layout/footer'],
   });
 
-  console.debug('Layout loaded:', this);
-}, JST);
+  /*
+   * ## menuLoaded
+   *
+   * Called when the menu has finished loading.
+   *
+   * @todo replace call with a serious event?
+   */
+  Layout.menuLoaded = function(menu) {
+    this._headerLayout.menu.show(menu);
+  };
 
-// display header and footer on app start
-Graviphoton.module('Layout').on("start", function(){
-  Graviphoton.header.show(new Graviphoton.Layout.Header());
-  Graviphoton.main.show(new Graviphoton.Layout.Main());
-  Graviphoton.footer.show(new Graviphoton.Layout.Footer());
-  console.debug('Layout started:', this);
-});
+  /*
+   * ## Initializer
+   *
+   * Show header and footers.
+   */
+  Layout.addInitializer(function() {
+    this._headerLayout = new Layout.Header();
 
+    Graviphoton.header.show(this._headerLayout);
+    Graviphoton.main.show(new Layout.Main());
+    Graviphoton.footer.show(new Layout.Footer());
+  });
+
+}, Graviphoton, JST);
